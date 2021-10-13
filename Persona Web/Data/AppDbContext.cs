a89ea07799d562181c6b3e884e50c44016d0a15c
Mache <mache.sanchezt@gmail.com>
2021-10-12 20:42:59 -0400

    Agregar archivos de proyecto.

---
+++ Persona Web/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Persona_Web.Models;
+
+namespace Persona_Web.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Persona_Web.Models.Person> Person { get; set; }
+    }
+}
---
+++ Persona Web/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Persona_Web.Models
+{
+    public enum SexType 
+    {
+        Female = 0,
+        Male = 1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required]
+        [StringLength(60,MinimumLength = 5, ErrorMessage = "Por favor ingrese entre 5 a 60 caracteres")]
+        [Display(Name ="Nombre de la Persona")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name ="Sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name = "Pasa Tiempo")]
+        public string Hobbie { get; set; }
+    }
+
+}
